3ca63a42aff229f7093271675b8a51199f47bb17
maaw92 <maziarz.maciej@gmail.com>
2026-02-05 11:02:21 +0100

    Init Commit

---
+++ FrontSite/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Text.Json.Serialization;
+
+namespace FrontSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        private string authUrl = "https://localhost:7163/api/auth/login";
+        public IActionResult Index()
+        {
+            var test = HttpContext.Session.GetString("token");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            RestClient client = new RestClient(authUrl);
+            RestRequest request = new RestRequest();
+            request.Method = Method.Post;
+            TokenRequest body = new TokenRequest() { Email = username, Password = password };
+            request.AddJsonBody(body);            
+            RestResponse response = client.Execute(request);
+            if(response.IsSuccessStatusCode)
+            {
+                TokenResponse token = JsonConvert.DeserializeObject<TokenResponse>(response.Content);
+                if (token == null)
+                    return RedirectToAction("Index", "Login");
+                HttpContext.Session.SetString("token", token.token);
+                return RedirectToAction("Index", "Movies");
+
+            }
+            return RedirectToAction("Index"); 
+        }
+        [HttpGet]
+        public IActionResult Logout(string username, string password)
+        {
+            HttpContext.Session.Remove("token");
+            //maybe could revoke token here.
+            return RedirectToAction("Index");
+        }
+
+    }
+    public class TokenResponse
+    {
+        public string token { get; set; }
+    }
+    public class TokenRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ FrontSite/Controllers/MovieController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
+
+namespace FrontSite.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private RestClient _client;
+        private string apiUrl = "https://localhost:7163/api/movies";
+
+        public MoviesController()
+        {
+            _client = new RestClient(apiUrl);
+            //_client.AddDefaultHeader("Authorization", $"Bearer {token}");
+        }
+        private bool IsTokenNull(string token)
+        {
+            return string.IsNullOrWhiteSpace(token);
+        }
+        public IActionResult Index()
+        {
+            string token = HttpContext.Session.GetString("token") ?? "";
+            if (string.IsNullOrWhiteSpace(token)) return RedirectToAction("Index", "Login");
+            RestRequest request = new RestRequest("/get");
+            request.Method = Method.Get;
+            request.AddHeader("Authorization", $"Bearer {token}");
+            var response = _client.Execute<List<MovieEventDto>>(request);            
+            if (!response.IsSuccessful)
+            {
+                if(response.StatusCode.Equals(HttpStatusCode.Unauthorized))
+                    return RedirectToAction("Index", "Login");
+                return RedirectToAction("Error", "Home");
+            }
+            return View(response.Data);
+        }
+        public IActionResult MyReservations()
+        {
+            string token = HttpContext.Session.GetString("token") ?? "";
+            if (string.IsNullOrWhiteSpace(token)) return RedirectToAction("Index", "Login");
+            RestRequest request = new RestRequest("/getExistingReservations");
+            request.Method = Method.Get;
+            request.AddHeader("Authorization", $"Bearer {token}");
+            var response = _client.Execute<List<ExistingEventDto>>(request);
+            if (!response.IsSuccessful)
+            {
+                if (response.StatusCode.Equals(HttpStatusCode.Unauthorized))
+                    return RedirectToAction("Index", "Login");
+                return RedirectToAction("Error", "Home");
+            }
+            return View(response.Data);
+        }
+
+        public IActionResult Seats(int id)
+        {
+            string token = HttpContext.Session.GetString("token") ?? "";
+            if (string.IsNullOrWhiteSpace(token)) return RedirectToAction("Index", "Login");
+            RestRequest request = new RestRequest($"/{id}/seats", Method.Get);
+            request.AddHeader("Authorization", $"Bearer {token}");
+            var response = _client.Execute<List<SeatDto>>(request);
+            if (!response.IsSuccessful)
+            {
+                if (response.StatusCode.Equals(HttpStatusCode.Unauthorized))
+                    return RedirectToAction("Index", "Login");
+                return RedirectToAction("Error", "Home");
+            }
+            ViewBag.EventId = id;
+            return View(response.Data);
+        }
+
+        [HttpPost]
+        public IActionResult Reserve(int eventId, int seatId)
+        {
+            var token = HttpContext.Session.GetString("token") ?? "";
+            if (string.IsNullOrWhiteSpace(token)) return RedirectToAction("Index", "Login");
+            RestRequest request = new RestRequest($"/{eventId}/reserve", Method.Post);            
+            request.AddHeader("Authorization", $"Bearer {token}");
+            request.AddJsonBody(new { SeatId = seatId });
+            var response = _client.Execute(request);
+            if (!response.IsSuccessful)
+                TempData["Error"] = "Błąd. Spróbuj ponownie.";
+            else
+                TempData["Success"] = "Rezerwacja udana.";
+            return RedirectToAction("Seats", new { id = eventId });
+        }
+    }
+    public class MovieEventDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+    public class ExistingEventDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string SeatNo { get; set; }
+    }
+
+    public class SeatDto
+    {
+        public int Id { get; set; }
+        public string SeatNumber { get; set; }
+    }
+}
